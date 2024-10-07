a4779bfe5e7a5f24c1cc1f6518a0289973e3e593
Klíber Benedek <165134282+Elydra555@users.noreply.github.com>
2024-10-07 14:20:03 +0200

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,18 @@
+//Ez a program kiszámolj a rombusz területét. Klíber Regő Benedek, 2024.09.30.
+
+/*
+* File: App.java
+* Author: Klíber Regő Benedek
+* Copyright: 2024, Klíber Regő Benedek
+* Group: Szoft II/I/N
+* Date: 2024-09-30
+* Github: https://github.com/Elydra5/
+* Licenc: GNU GPL
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Rombter rt = new Rombter();
+        rt.RombuszTerulet();
+    }
+}
